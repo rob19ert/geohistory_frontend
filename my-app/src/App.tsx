4020e63af8be5c37d1e686291b6ad5ab7e754c90
admin <rsay3@bk.ru>
2025-02-02 13:09:18 +0300

    tauri

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -3,8 +3,26 @@ import { ROUTES } from "./Routes";
 import { HomePage } from "./pages/HomePage";
 import DiscovererListPage from "./pages/DiscovererList";
 import { DiscovererDetailPage } from "./pages/DiscovererDetail";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    if (typeof window !== 'undefined' && (window as any).__TAURI__?.tauri) {
+      const { invoke } = (window as any).__TAURI__.tauri;
+      
+      invoke('tauri', { cmd: 'create' })
+        .then((response: any) => console.log(response))
+        .catch((error: any) => console.log(error));
+
+      return () => {
+        invoke('tauri', { cmd: 'close' })
+          .then((response: any) => console.log(response))
+          .catch((error: any) => console.log(error));
+      };
+    }
+  }, []);
+   
+
   return (
     <BrowserRouter>
       <Routes>
